6255ad2aee5088d1a0b3da76a2438978f000541c
Ramil <tlsmscience@gmail.com>
2024-05-31 18:53:15 +0500

    Удаление лишнего

--- users/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class UsersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'users'
-
-
-(lambda x, y : print(x*y))(3,6)
