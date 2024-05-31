fec11dbbe983d2c4373b272dddd9d2ab8108d970
Ramil <tlsmscience@gmail.com>
2024-05-31 18:53:15 +0500

    Снова возврат далеко куда

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,10 @@
-
 from django.contrib import admin
 from django.urls import path, include
 
+# from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("polls/", include ("polls.urls")),
+    path('', include('blog.urls')),
+    path('', include('users.urls'))
 ]
