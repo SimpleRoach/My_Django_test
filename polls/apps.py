2d6ba5ced55112c8fec5926319b3c4674de4763e
Ramil <tlsmscience@gmail.com>
2024-05-31 20:39:47 +0500

    Наладил файл приложений и настроек. Уже запускается, но не работает

--- polls/apps.py
+++ polls/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class BlogConfig(AppConfig):
+class PollsConfig1(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'blog'
+    name = 'polls'
