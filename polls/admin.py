a2b90b24f4fe51c287f23ef31def4419deeb0b64
Ramil <tlsmscience@gmail.com>
2024-06-03 18:08:32 +0500

    Улучшил админ менюшку

--- polls/admin.py
+++ polls/admin.py
@@ -3,12 +3,15 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         ("Вопрос", {'fields':['question_text']}),
         ("Настройка времени", {'fields':["pub_date"]})
     ]
-    # fields = ["pub_date", "question_text"]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
+
